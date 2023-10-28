c93c7aaecca0e7fc471f8f865b7d55f6a27efe2f
spython <emmanuelegbo01@gmail.com>
2023-10-28 15:29:31 +0100

    commit

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -4,7 +4,26 @@ export default function Profile() {
   return (
     <div className='profile'>
         <div className="profileWrapper">
-            
+            <div className="profileCover">
+                <img src="/assets/noAvatar.png" alt="no avatar" className="profileUserImg" />
+            </div>
+
+            <div className="profileInfo">
+                <h4>emmy manny</h4>
+                <span className='spanChoice'>Creative</span>
+                <span className='spanCareer'>acting</span>
+
+                <div className="follow">
+                    <span className="follow1">
+                        <span className='fol1'>0</span> followers
+                    </span>
+                    <span className="follow2">
+                        <span className='fol2'>0</span> following
+                    </span>
+                </div>   
+            </div>
+
+            <button className="edit">Edit Profile</button>
         </div>
     </div>
   )
