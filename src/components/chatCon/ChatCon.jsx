a8099d49042c959e967cf1d2d35b267fdcedaacb
spython <emmanuelegbo01@gmail.com>
2023-11-01 08:46:53 +0100

    commit

--- src/components/chatCon/ChatCon.jsx
+++ src/components/chatCon/ChatCon.jsx
@@ -1,7 +1,29 @@
+import { Search } from '@mui/icons-material'
 import './chatCon.scss'
+import { Link } from 'react-router-dom'
 
 export default function ChatCon() {
   return (
-    <div className='chatCon'>ChatCon</div>
+    <div className='chatCon'>
+        <div className='wrapper'>
+            <div className="top">
+                <Link to='/chat' className='link'><span>Chats</span></Link>
+                <div className="chatSearch">
+                    <Search className='chatSearchIcon'/>
+                    <input type="text" placeholder='search chat' className="chatInput" />
+                </div>
+            </div>
+
+            <div className="bottom">
+                <span>You do not have any Chat</span>
+                <span className='connect'>
+                    Connect with other experts and enjoy chats with them.
+                    You can start from your category, it is easier that way.
+                </span>
+
+                <b>Start Connecting!</b>
+            </div>
+        </div>
+    </div>
   )
 }
