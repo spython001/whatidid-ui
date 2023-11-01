5e7bc47bdc6f43c45f727c818d36c43e3193e460
spython <emmanuelegbo01@gmail.com>
2023-11-01 09:32:35 +0100

    commit

--- src/components/chatProfile/ChatProfile.jsx
+++ src/components/chatProfile/ChatProfile.jsx
@@ -1,7 +1,23 @@
 import './chatProfile.scss'
+import { Link } from 'react-router-dom'
 
 export default function ChatProfile() {
   return (
-    <div className='chatProfile'>ChatProfile</div>
+    <div className='chatProfile'>
+        <div className='wrapper'>
+            <div className="top">
+                <Link to='/profile' className='link'><img src="/assets/noAvatar.png" alt="no avatar" /></Link>
+                <div className='spanTop'>
+                    <span className='welcome'>Welcome Back!</span>
+                    <span className='name'>emmy manny</span>
+                </div>
+            </div>
+
+            <div className='bottom'>
+                <span className='spanBottom'>Get back on track. Start connecting with talents. Lets go</span>
+                <img src='/assets/chatPic.png' alt='chat pic'/>
+            </div>
+        </div>
+    </div>
   )
 }
