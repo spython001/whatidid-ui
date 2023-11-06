3d3da691d1b694db43732a21648036cfc653dc81
spython <emmanuelegbo01@gmail.com>
2023-11-06 13:58:47 +0100

    commit

---
+++ src/components/schedule/Schedule.jsx
@@ -0,0 +1,26 @@
+import { HeadsetMic } from '@mui/icons-material'
+import './schedule.scss'
+
+export default function Schedule() {
+  return (
+    <div className='recorded'>
+      <div className="wrapper">
+        <div className="top">
+          <span className='topStuff'><HeadsetMic/>  <div className='record'>Recorded</div></span>
+          <span>Real test</span>
+          <span className='course'>science</span>
+          <span className='date'>Recorded on: April 12, 2023 @ 5:28 PM</span>
+        </div>
+
+        <div className="bottom">
+          <div className='bottomStuff'>
+            <img src="/assets/noAvatar.png" alt="no avatar" />
+            <span className='name'>Mike Menser</span>
+            <span className="host">Host</span>
+          </div>
+          <h4>passionate about code :)</h4>
+        </div>
+      </div>
+    </div>
+  )
+}
